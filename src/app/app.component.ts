dbab23c0456ba29d24ffb9788853c56de2378fd1
Gustavo Sampaio <sampaioglima@gmail.com>
2021-12-22 13:21:41 -0300

    rewrite the default app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  appName: string = 'ACME Product Management';
 }
