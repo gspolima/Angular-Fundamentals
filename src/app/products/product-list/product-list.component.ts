cfe5a91a54448cc1f1900aa9054096cdc2f587c1
Gustavo Sampaio <sampaioglima@gmail.com>
2021-12-22 19:29:37 -0300

    sketch the product list component and register it

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pm-product-list',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+    title: string = "Product List";
+}
