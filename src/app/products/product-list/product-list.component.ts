fb510b8aaa36625de38105766673daf1f0ee526a
Gustavo Sampaio <sampaioglima@gmail.com>
2021-12-23 17:37:16 -0300

    event binding, two-way data binding and pipes

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -8,13 +8,15 @@ export class ProductListComponent {
     pageTitle: string = "Product List";
     imageWidthEm: number = 4;
     imageMarginEm: number = 0.5;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
 
     products: any[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
             "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
+            "releaseDate": "2021-03-18",
             "description": "15 gallon capacity rolling garden cart",
             "price": 32.99,
             "starRating": 4.2,
@@ -24,11 +26,19 @@ export class ProductListComponent {
             "productId": 5,
             "productName": "Hammer",
             "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
+            "releaseDate": "2021-05-21",
             "description": "Curved claw steel hammer",
             "price": 8.9,
             "starRating": 4.8,
             "imageUrl": "assets/images/hammer.png"
           }
     ];
+
+    onToggleImage(): void {
+      this.showImage = !this.showImage;
+    }
+
+    formatPrice(price: number): string {
+      return `$${price.toFixed(2)}`;
+    }
 }
