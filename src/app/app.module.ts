21bd0100a79f0cd087d3f5560c70d9b18b63f4b7
Gustavo Sampaio <sampaioglima@gmail.com>
2021-12-23 17:35:17 -0300

    make the pt locale available everywhere

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -10,8 +16,15 @@ import { ProductListComponent } from './products/product-list/product-list.compo
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    { 
+      provide: LOCALE_ID,
+      useValue: 'pt' 
+    }
+  ]
 })
 export class AppModule { }
